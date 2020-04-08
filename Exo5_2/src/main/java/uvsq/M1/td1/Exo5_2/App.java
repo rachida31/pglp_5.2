7e8e107c54108e709138db3681b621f233b42231
Rachida <rachidaouchene1@gmail.com>
2020-04-08 06:47:19 +0200

    creation de projet maven

---
+++ Exo5_2/src/main/java/uvsq/M1/td1/Exo5_2/App.java
@@ -0,0 +1,13 @@
+package uvsq.M1.td1.Exo5_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
